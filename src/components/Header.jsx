f18f2d8744fb13fca4420255817cb216b39f1aef
ajnanmvr <ajnanpulikkathody@gmail.com>
2023-10-13 23:36:54 +0530

    searchbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
-import React,{useState} from "react";
-import { Link, useLocation,useNavigate  } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 function Header() {
   const location = useLocation();
@@ -114,25 +114,22 @@ function Header() {
                 <div style={{ position: "relative" }}>
                   <input
                     type="text"
-                    className="border-2 p-2 border-primary rounded-xl min-w-[30vw]"
+                    className="border-2 px-4 py-2 border-primary rounded-xl min-w-[30vw]"
+                    placeholder="Search Here"
                     value={searchText}
                     onChange={(e) => setSearchText(e.target.value)}
                   />
-   
-                    <button
-                      onClick={handleGoBack}
-                      style={{
-                        position: "absolute",
-                        top: 0,
-                        right: 0,
-                        background: "none",
-                        border: "none",
-                        cursor: "pointer",
-                      }}
-                    >
-                      &times;
-                    </button>
 
+                  <button onClick={handleGoBack} className="-m-8">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="1em"
+                      viewBox="0 0 384 512"
+                      className="fill-slate-400 hover:fill-primary"
+                    >
+                      <path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" />
+                    </svg>
+                  </button>
                 </div>
               </li>
             )}
